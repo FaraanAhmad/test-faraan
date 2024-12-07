ebe0a9ad711017a7e95187832618bc094a4e52db
FaraanAhmad <FaraanAhmadBinRidzuan@Gmail.com>
2024-12-07 16:09:36 +0800

    Completed the setup of testing environment

---
+++ index-test.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var bodyParser = require("body-parser");
+var app = express();
+
+const PORT = process.env.PORT || 5050
+var startPage = "index.html";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("./instrumented"));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/instrumented/" + startPage);
+})
+
+const { addMovie} = require('./utils/MovieUilAlz');
+app.post('/add-resource', addMovie);
+
+const {  editMovie } = require('./utils/MovieUilFaraan');
+app.put('/edit-movie/:id', editMovie);
+
+const { viewResources } = require('./utils/MovieUilFir');
+app.get('/view-resource/:search', viewResources);
+
+
+server = app.listen(PORT, function () {
+    const address = server.address();
+    const baseUrl = `http://${address.address == "::" ? 'localhost' : address.address}:${address.port}`;
+    console.log(`Demo project at: ${baseUrl}`);
+});
+
+module.exports = {app, server,}
